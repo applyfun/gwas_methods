3ae1aa1eac051eb3a5722537258c8b9c75f0da12
applyfun <ryan.arathimos@kcl.ac.uk>
2021-02-21 15:08:07 +0000

    Edited code annotations

--- regenie_gwas_step1.sh
+++ regenie_gwas_step1.sh
@@ -9,7 +9,7 @@
 
 echo "Beginning job!"
 
-echo "Ivybridge!"
+echo "Ivybridge chipset used!"
 
 cd ~/brc_scratch/scripts/
 
@@ -20,46 +20,44 @@ samplepath=$(sed '17q;d' paths.txt)
 echo $basepath
 echo $ukbpath
 
+# set output path
 outputbasepath=${basepath}output/biomarkers_ukb/
-softwarebasepath=${basepath}software/
 
 echo $outputbasepath
-echo $softwarebasepath
 
 echo "Paths set!"
 
-#run regenie
-cd ${softwarebasepath}
+# run regenie
 
-echo "Beginning job!"
-
-cd  ${softwarebasepath}regenie
+cd  ${outputbasepath}regenie
 
 # regenie doesn't like bim files with chrom nums greater than 24
 
-#module load apps/plink/1.9.0b6.10
+module load apps/plink/1.9.0b6.10
 
-#plink --bfile ${ukbpath}genotyped/ukb18177_glanville_binary_pre_qc --chr 1-22,X --make-bed --out ${outputbasepath}tmp_ukb18177_glanville_binary_pre_qc_no26
+plink --bfile ${ukbpath}genotyped/ukb18177_glanville_binary_pre_qc --chr 1-22,X --make-bed --out ${outputbasepath}tmp_ukb18177_glanville_binary_pre_qc_no26
 
 # load regenie module
 
 module load utilities/use.dev
 module load apps/regenie/2.0.1-mkl
 
-#regenie \
-#  --step 1 \
-#  --bed ${outputbasepath}tmp_ukb18177_glanville_binary_pre_qc_no26 \
-#  --extract ${ukbpath}2019_new_qc/ukb18177_glanville_post_qc_snp_list.txt \
-#  --phenoFile ${outputbasepath}prevalent_trd_gpcontrols_plink_pheno.txt \
-#  --remove ${outputbasepath}bolt_remove_IIDs_with_negatives_plink_file.txt \
-#  --bsize 1000 \
-#  --bt --lowmem \
-#  --lowmem-prefix ${outputbasepath}tmp_rg \
-#  --out ${outputbasepath}fit_bin_test
+regenie \
+  --step 1 \
+  --bed ${outputbasepath}tmp_ukb18177_glanville_binary_pre_qc_no26 \
+  --extract ${ukbpath}2019_new_qc/ukb18177_glanville_post_qc_snp_list.txt \
+  --phenoFile ${outputbasepath}prevalent_trd_gpcontrols_plink_pheno.txt \
+  --remove ${outputbasepath}bolt_remove_IIDs_with_negatives_plink_file.txt \
+  --bsize 1000 \
+  --bt --lowmem \
+  --lowmem-prefix ${outputbasepath}tmp_rg \
+  --out ${outputbasepath}fit_bin_test
+
+# submit step2 job array - comment out if step 2 to be submitted manually
 
 sbatch regenie_gwas_step2.sh
 
-echo Step1Complete
+echo FinishedStep1
 
 #
 
