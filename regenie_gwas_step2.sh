6fd131a7045386d021438316e54bf4d8229c0717
applyfun <ryan.arathimos@kcl.ac.uk>
2021-06-04 10:06:27 +0100

    Removed split option

--- regenie_gwas_step2.sh
+++ regenie_gwas_step2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-#SBATCH --time=24:00:00  
+#SBATCH --time=12:00:00  
 #SBATCH --job-name=regenie_step2_array 
 #SBATCH --partition brc,shared
 #SBATCh --output=array_regenie.%A_%a.out
@@ -15,7 +15,6 @@ cd ~/brc_scratch/scripts/
 basepath=$(sed '8q;d' paths.txt)
 ukbpath=$(sed '14q;d' paths.txt)
 samplepath=$(sed '17q;d' paths.txt)
-
 outputbasepath=${basepath}output/biomarkers_ukb/
 
 echo Basepath $basepath
@@ -26,6 +25,7 @@ echo Samplepath$samplepath
 echo "Paths set!"
 
 # load regenie module and assign array task ID as Chromosome number
+
 echo ${SLURM_ARRAY_TASK_ID}
 
 CHR=${SLURM_ARRAY_TASK_ID}
@@ -35,24 +35,23 @@ echo ${CHR}
 module load utilities/use.dev
 module load apps/regenie/2.0.1-mkl
 
-echo ${CHR}
+cd ${outputbasepath}
 
 regenie \
   --step 2 \
   --bgen ${ukbpath}imputed/ukb18177_glanville_imp_chr${CHR}_MAF1_INFO4_v1.bgen \
   --sample ${samplepath}ukb1817_imp_chr1_v2_s487398.sample \
   --covarFile ${outputbasepath}flashpca_20pcs_ukb.tab \
-  --phenoFile ${outputbasepath}prevalent_trd_gpcontrols_plink_pheno.txt \
+  --phenoFile ${outputbasepath}prevalent_trd_mddcontrols_plink_pheno.txt \
   --remove ${outputbasepath}bolt_remove_IIDs_with_negatives_plink_file.txt \
   --bsize 400 \
   --bt \
-  --firth --approx \
-  --pThresh 0.01 \
+  --spa --approx \
+  --pThresh 0.05 \
   --minINFO 0.6 \
   --minMAC 10 \
-  --pred ${outputbasepath}fit_bin_test_pred.list \
-  --split \
-  --out bin_${CHR}_out_firth
+  --pred ${outputbasepath}fit_bin_trd_mddcontrols_pred.list \
+  --out ${outputbasepath}bin_${CHR}_out_spa_trd_mdd_controls
 
 #
 
